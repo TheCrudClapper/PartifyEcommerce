TheCrudClapper/PartifyEcommerce

commit c7dd22924f3291f60ffbb1e2cedb30941eea6c28

refactor: refactor IPictureHandler service and IConfiguratioReader to support checks whether images exist in directory

PartifyEcommerce/Partify.Core/Mappings/ToDto/OfferMappings.cs
@@ -1,5 +1,4 @@
 ﻿using CSOS.Core.Domain.Entities;
-using CSOS.Core.DTO;
 using CSOS.Core.DTO.DeliveryTypeDto;
 using CSOS.Core.DTO.OfferDto;
 using CSOS.Core.DTO.UniversalDto;

PartifyEcommerce/Partify.UI/Controllers/OfferController.cs
@@ -176,7 +176,7 @@ namespace CSOS.UI.Controllers
             _logger.LogInformation("OfferControler - GET UserOffers Method called with parameters: Title: {Title}", title);
             IEnumerable<UserOfferResponse> response = await _offerService.GetFilteredUserOffers(title);
             IEnumerable<UserOffersViewModel> userOffers = response
-                .Select(item => item.ToUserOffersViewModel(_configurationReader));
+                .Select(item => item.ToUserOffersViewModel(_pictureHandlerService));
 
             return View(userOffers);
         }
@@ -187,7 +187,7 @@ namespace CSOS.UI.Controllers
             _logger.LogInformation("OfferControler - GET FilterUserOffers Method called with parameters: Title: {Title}", title);
             IEnumerable<UserOfferResponse> response = await _offerService.GetFilteredUserOffers(title);
             IEnumerable<UserOffersViewModel> viewModel = response
-                .Select(item => item.ToUserOffersViewModel(_configurationReader));
+                .Select(item => item.ToUserOffersViewModel(_pictureHandlerService));
             return PartialView("OfferPartials/_UserOfferListPartial", viewModel);
         }
 
@@ -204,7 +204,7 @@ namespace CSOS.UI.Controllers
                 return View("Error", response.Error.Description);
             }
                
-            var viewModel = response.Value.ToOfferDetailsViewModel(_configurationReader);
+            var viewModel = response.Value.ToOfferDetailsViewModel(_pictureHandlerService);
             return View(viewModel);
         }
 
@@ -216,7 +216,7 @@ namespace CSOS.UI.Controllers
             IEnumerable<OfferIndexResponse> filteredOffers = await _offerService.GetFilteredOffers(filter);
             OfferIndexViewModel viewModel = new OfferIndexViewModel()
             {
-                Items = filteredOffers.Select(item => item.ToOfferIndexItemViewModel(_configurationReader))
+                Items = filteredOffers.Select(item => item.ToOfferIndexItemViewModel(_pictureHandlerService))
                     .ToList(),
 
                 DeliveryOptions = (await _deliveryTypeGetterService.GetAllDeliveryTypesAsSelectionList())
@@ -236,7 +236,7 @@ namespace CSOS.UI.Controllers
         {
             _logger.LogInformation("OfferControler - GET FilterOffers Method called with parameters: {@OfferFilter}", filter);
             IEnumerable<OfferIndexResponse> filteredOffers = await _offerService.GetFilteredOffers(filter);
-            var viewModel = filteredOffers.Select(item => item.ToOfferIndexItemViewModel(_configurationReader));
+            var viewModel = filteredOffers.Select(item => item.ToOfferIndexItemViewModel(_pictureHandlerService));
             return PartialView("OfferPartials/_OfferListPartial", viewModel);
         }
     }

PartifyEcommerce/Partify.UI/Mappings/ToViewModel/OfferViewModelMappings.cs
@@ -3,7 +3,6 @@ using CSOS.Core.DTO.OfferDto;
 using CSOS.UI.Mappings.Universal;
 using CSOS.UI.ViewModels.DeliveryTypeViewModels;
 using CSOS.UI.ViewModels.OfferViewModels;
-using CSOS.UI.ViewModels.SharedViewModels;
 
 namespace CSOS.UI.Mappings.ToViewModel
 {
@@ -27,7 +26,7 @@ namespace CSOS.UI.Mappings.ToViewModel
             };
         }
 
-        public static OfferIndexItemViewModel ToOfferIndexItemViewModel(this OfferIndexResponse dto, IConfigurationReader configurationReader)
+        public static OfferIndexItemViewModel ToOfferIndexItemViewModel(this OfferIndexResponse dto, IPictureHandlerService pictureHandlerService)
         {
             return new OfferIndexItemViewModel
             {
@@ -36,7 +35,7 @@ namespace CSOS.UI.Mappings.ToViewModel
                 ProductName = dto.ProductName,
                 Id = dto.Id,
                 DateCreated = dto.DateCreated,
-                ImageUrl = string.IsNullOrEmpty(dto.ImageUrl) ? configurationReader.DefaultProductImage : dto.ImageUrl,
+                ImageUrl = pictureHandlerService.ReplaceImageIfNotFound(dto.ImageUrl),
                 ProductCategory = dto.ProductCategory,
                 ProductCondition = dto.ProductCondition,
                 StockQuantity = dto.StockQuantity,
@@ -44,7 +43,7 @@ namespace CSOS.UI.Mappings.ToViewModel
             };
         }
         
-        public static OfferDetailsViewModel ToOfferDetailsViewModel(this OfferResponse dto, IConfigurationReader configurationReader)
+        public static OfferDetailsViewModel ToOfferDetailsViewModel(this OfferResponse dto, IPictureHandlerService pictureHandlerService)
         {
             
             return new OfferDetailsViewModel
@@ -67,17 +66,17 @@ namespace CSOS.UI.Mappings.ToViewModel
                 Seller= dto.Seller,
                 ProductCategory = dto.ProductCategory,
                 StockQuantity = dto.StockQuantity,
-                ProductImages = dto.ProductImages.Select(img => string.IsNullOrEmpty(img) ? configurationReader.DefaultProductImage : img).ToList(),
+                ProductImages = dto.ProductImages.Select(img => pictureHandlerService.ReplaceImageIfNotFound(img)).ToList(),
             };
         }
 
-        public static UserOffersViewModel ToUserOffersViewModel(this UserOfferResponse dto, IConfigurationReader configurationReader)
+        public static UserOffersViewModel ToUserOffersViewModel(this UserOfferResponse dto, IPictureHandlerService pictureHandlerService)
         {
             return new UserOffersViewModel
             {
                 DateCreated = dto.DateCreated,
                 Id = dto.Id,
-                ImageUrl = string.IsNullOrEmpty(dto.ImageUrl) ? configurationReader.DefaultProductImage : dto.ImageUrl,
+                ImageUrl = pictureHandlerService.ReplaceImageIfNotFound(dto.ImageUrl),
                 Price = dto.Price,
                 ProductCategory = dto.ProductCategory,
                 ProductCondition = dto.ProductCondition,

commit 5cf169121182411f93adcf3dd242dd1f36c4024c

refactor: refactor image deletion logic and mappings
Fixed a bug introduced in c7dd229 commit.
Bug doesn't allow user to delete image from database if original image was swapped by placeholder.

PartifyEcommerce/Partify.Core/DTO/OfferDto/OfferUpdateRequest.cs
@@ -12,11 +12,11 @@ namespace CSOS.Core.DTO.OfferDto
         public bool IsOfferPrivate { get; set; }
         public int SelectedProductCondition { get; set; }
         public List<IFormFile> UploadedImages { get; set; } = null!;
-        public List<string>? UploadedImagesUrls { get; set; }
+        public List<string> UploadedImagesUrls { get; set; } = null!;
         public decimal Price { get; set; }
         public int StockQuantity { get; set; }
         public List<int> SelectedOtherDeliveries { get; set; } = null!;
-        public List<string>? ImagesToDelete { get; set; } = new List<string>();
+        public List<int>? ImagesToDeleteIds { get; set; } = new List<int>();
         public int? SelectedParcelLocker { get; set; }
     }
 }

PartifyEcommerce/Partify.Core/Mappings/ToDto/OfferMappings.cs
@@ -99,7 +99,7 @@ namespace CSOS.Core.Mappings.ToDto
                   .Where(item => item.IsActive)
                   .Select(item => new SelectListItemDto()
                   {
-                      Value = item.ImagePath,
+                      Value = item.Id.ToString(),
                       Text = item.ImagePath,
                   })
                   .ToList(),

PartifyEcommerce/Partify.Core/ServiceContracts/IProductImageService.cs
@@ -1,5 +1,4 @@
 ﻿using CSOS.Core.Domain.Entities;
-using CSOS.Core.DTO;
 using CSOS.Core.DTO.UniversalDto;
 using CSOS.Core.ResultTypes;
 
@@ -21,6 +20,6 @@ namespace CSOS.Core.ServiceContracts
         /// <param name="images">List of product images</param>
         /// <param name="imageUrls">List of image URLs to delete.</param>
         /// <returns>A task representing the asynchronous operation.</returns>
-        Result DeleteImagesFromOffer(IEnumerable<ProductImage> images, IEnumerable<string> imageUrls);
+        Result DeleteImagesFromOffer(IEnumerable<ProductImage> images, IEnumerable<int>? imageIds);
     }
 }

PartifyEcommerce/Partify.Core/Services/OfferService.cs
@@ -84,13 +84,11 @@ namespace CSOS.Core.Services
             product.ProductCategoryId = updateRequest.SelectedProductCategory;
 
             //deletes images checked by user
-            if (updateRequest.ImagesToDelete?.Count > 0)
-            {
-                var result = _productImageService.DeleteImagesFromOffer(product.ProductImages, updateRequest.ImagesToDelete);
+            var pictureDeleteResult = _productImageService.DeleteImagesFromOffer(
+                product.ProductImages.Where(item => item.IsActive), updateRequest.ImagesToDeleteIds);
 
-                if (result.IsFailure)
-                    return Result.Failure(result.Error);
-            }
+            if (pictureDeleteResult.IsFailure)
+                return Result.Failure(pictureDeleteResult.Error);
 
             await SaveNewImagesAsync(updateRequest, product);
 

PartifyEcommerce/Partify.Core/Services/ProductImageService.cs
@@ -1,5 +1,4 @@
 ﻿using CSOS.Core.Domain.Entities;
-using CSOS.Core.Domain.InfrastructureServiceContracts;
 using CSOS.Core.Domain.RepositoryContracts;
 using CSOS.Core.DTO.UniversalDto;
 using CSOS.Core.Mappings.ToDto;
@@ -23,19 +22,23 @@ namespace CSOS.Core.Services
             return items.Select(item => item.ToSelectListItem()).ToList();
         }
         
-        public Result DeleteImagesFromOffer(IEnumerable<ProductImage> images, IEnumerable<string> imageUrls)
+        public Result DeleteImagesFromOffer(IEnumerable<ProductImage> images, IEnumerable<int>? imageIds)
         {
             if (!images.Any())
                 return Result.Failure(ProductImageErrors.ProductImagesAreEmpty);
 
-            foreach (var imageToDelete in images)
+            if(imageIds != null)
             {
-                if (imageUrls.Any(url => imageToDelete.ImagePath.Contains(url, StringComparison.OrdinalIgnoreCase)))
+                foreach (var imageToDelete in images)
                 {
-                    imageToDelete.DateDeleted = DateTime.UtcNow;
-                    imageToDelete.IsActive = false;
+                    if (imageIds.Contains(imageToDelete.Id))
+                    {
+                        imageToDelete.DateDeleted = DateTime.UtcNow;
+                        imageToDelete.IsActive = false;
+                    }
                 }
             }
+
             return Result.Success();
         }
     }

PartifyEcommerce/Partify.UI/Controllers/OfferController.cs
@@ -122,6 +122,7 @@ namespace CSOS.UI.Controllers
                 _logger.LogWarning("Invalid model state. Errors: {Errors}",
                     string.Join(", ", ModelState.Values.SelectMany(item => item.Errors).Select(item => item.ErrorMessage)));
                 await _offerViewModelInitializer.InitializeAllAsync(viewModel);
+                await _offerViewModelInitializer.GetOfferPicturseAsync(viewModel);
                 return View(viewModel);
             }
 

PartifyEcommerce/Partify.UI/Filters/ValidatePicturesFilter.cs
@@ -40,7 +40,7 @@ namespace CSOS.UI.Filters
         }
         private bool HasAtLeastOneImage(EditOfferViewModel vm, bool newImagesValid)
         {
-            int remaining = vm.ExistingImagesCount - (vm.ImagesToDelete?.Count ?? 0);
+            int remaining = vm.ExistingImagesCount - (vm.ImagesToDeleteIds?.Count ?? 0);
             bool hasNew = newImagesValid && vm.UploadedImages?.Any() == true;
             return remaining > 0 || hasNew;
         }

PartifyEcommerce/Partify.UI/Helpers/OfferViewModelInitializer.cs
@@ -33,8 +33,8 @@ namespace CSOS.UI.Helpers
             await PopulateCategories<TViewModel>(viewModel);
             await PopulateConditions<TViewModel>(viewModel);
 
-            if (viewModel is EditOfferViewModel editVm)
-                await GetOfferPicturseAsync(editVm);
+            //if (viewModel is EditOfferViewModel editVm)
+            //    await GetOfferPicturseAsync(editVm);
 
         }
 

PartifyEcommerce/Partify.UI/Mappings/ToDto/OfferDtoMappings.cs
@@ -16,7 +16,9 @@ namespace CSOS.UI.Mappings.ToDto
                 ProductName = viewModel.ProductName,
                 StockQuantity = viewModel.StockQuantity,
                 UploadedImages = viewModel.UploadedImages,
-                ImagesToDelete = viewModel.ImagesToDelete,
+                ImagesToDeleteIds = viewModel.ImagesToDeleteIds?
+                    .Where(item => int.TryParse(item, out _))
+                    .Select(int.Parse).ToList(),
                 SelectedParcelLocker = viewModel.SelectedParcelLocker,
                 SelectedProductCondition = int.Parse(viewModel.SelectedProductCondition),
                 SelectedProductCategory = int.Parse(viewModel.SelectedProductCategory),

PartifyEcommerce/Partify.UI/Mappings/ToViewModel/OfferViewModelMappings.cs
@@ -24,8 +24,8 @@ namespace CSOS.UI.Mappings.ToViewModel
                 SelectedProductCategory = dto.SelectedProductCategory,
                 ExistingImagesUrls = dto.ExistingImagesUrls!.Select(item => new SelectListItem
                 {
-                    Text = item.Text,
-                    Value = pictureHandlerService.ReplaceImageIfNotFound(item.Value)
+                    Text = pictureHandlerService.ReplaceImageIfNotFound(item.Text),
+                    Value = item.Value,
                 })
                 .ToList(),
                 ExistingImagesCount = dto.ExistingImagesUrls?.Count ?? 0,

PartifyEcommerce/Partify.UI/Mappings/Universal/ToSelectListItemMappings.cs
@@ -1,4 +1,5 @@
-﻿using CSOS.Core.Domain.InfrastructureServiceContracts;
+﻿using ComputerServiceOnlineShop.Services;
+using CSOS.Core.Domain.InfrastructureServiceContracts;
 using CSOS.Core.DTO.UniversalDto;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -27,8 +28,8 @@ namespace CSOS.UI.Mappings.Universal
 
             return items.Select(item => new SelectListItem
             {
-                Text = item.Text,
-                Value = pictureHandlerService.ReplaceImageIfNotFound(item.Value),
+                Text = pictureHandlerService.ReplaceImageIfNotFound(item.Value),
+                Value = item.Value,
             })
             .ToList();
         }

PartifyEcommerce/Partify.UI/ViewModels/OfferViewModels/EditOfferViewModel.cs
@@ -22,6 +22,6 @@ namespace CSOS.UI.ViewModels.OfferViewModels
         public List<SelectListItem>? ExistingImagesUrls { get; set; } = new List<SelectListItem>();
 
         //Images selected by user for deletion
-        public List<string>? ImagesToDelete { get; set; } = new List<string>();
+        public List<string>? ImagesToDeleteIds { get; set; } = new List<string>();
     }
 }

PartifyEcommerce/Partify.UI/Views/Offer/Edit.cshtml
@@ -55,9 +55,9 @@
                     {
                         <div class="col-6 col-sm-4 col-md-3 col-lg-2">
                             <div class="card border-0 shadow-sm position-relative">
-                                <img src="@imageUrl.Value" alt="image" class="img-fluid rounded" style="height: 150px; object-fit: cover;">
+                                <img src="@imageUrl.Text" alt="image" class="img-fluid rounded" style="height: 150px; object-fit: cover;">
                                 <div class="form-check position-absolute top-0 end-0 m-2">
-                                    <input class="form-check-input" type="checkbox" name="ImagesToDelete" value="@imageUrl.Value" title="Mark to delete" />
+                                    <input class="form-check-input" type="checkbox" name="ImagesToDeleteIds" value="@imageUrl.Value" title="Mark to delete" />
                                 </div>
                             </div>
                         </div>